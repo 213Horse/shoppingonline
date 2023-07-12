a691f79e8f366b08b00182a96bcce7265204cb4c
tours <minhquando59@gmail.com>
2023-07-12 08:45:16 +0700

    first commit

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster0.4z6ih3a.mongodb.net',
+    DB_USER: 'minhquando59',
+    DB_PASS: '8gxp44nLjjjATwrQ',
+    DB_DATABASE: 'shoppingonline',
+    EMAIL_USER: '<email_user>', // Microsoft mail service
+    EMAIL_PASS: '<email_pass>',
+    JWT_SECRET: '<jwt_secret>',
+    JWT_EXPIRES: '86400000', // in milliseconds
+  };
+  module.exports = MyConstants;
